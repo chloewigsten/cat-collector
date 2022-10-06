a1399b17807e0a51c6d5f384e2b74b877339e7f4
chloewigsten <chloewigsten@gmail.com>
2022-10-06 11:46:58 -0400

    add procfile and requirements

--- cat_collector_project/settings.py
+++ cat_collector_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -128,3 +127,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'main_app/static')]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+import django_heroku
+django_heroku.settings(locals())
